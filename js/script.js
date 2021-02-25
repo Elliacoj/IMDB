9b08d1e2da2c8fc74bfad0e6eb484eedafc75908
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2021-02-25 14:46:21 +0100

    Modif Ajout d'image

--- js/script.js
+++ js/script.js
@@ -9,22 +9,21 @@ xhr.responseType = "json";
 xhr.onload = function() {
     if(xhr.status === 200){
         let response = xhr.response;
-        for(let x = 1; x < 7; x++) {
-            arrayFeature.push(response[0][x][0]["feature"]);
-            console.log(arrayFeature.length);
+
+        for(let x = 0; x < 6; x++) {
+            arrayFeature.push(response[0][x][0]["features"][1]);
         }
 
-        for(let x = 2; x < 7; x++) {
-            arrayFeatureUp.push(response[0][x][0]["feature"]);
-            console.log(arrayFeature.length);
+        for(let x = 1; x < 6; x++) {
+            arrayFeatureUp.push(response[0][x][0]["features"][0]);
         }
-        arrayFeatureUp.push(response[0][1][0]["feature"])
+        arrayFeatureUp.push(response[0][0][0]["features"][0])
 
-        let carrouselMain = new Carrousel(arrayFeature, "principal", document.getElementById("carrouselLeft"), "100%", "100%", 40  ,1, true, "left");
+        let carrouselMain = new Carrousel(arrayFeature, "principal", document.getElementById("carrouselLeft"), "100%", "100%", 80, 1, true, "left");
         carrouselMain.createCarrousel(5, 10);
         carrouselMain.start();
 
-        let carrouselMainExt = new Carrousel(arrayFeatureUp, "secondary", document.getElementById("carrouselUp"), "80%", "85%", 40 ,3, false, "up");
+        let carrouselMainExt = new Carrousel(arrayFeatureUp, "secondary", document.getElementById("carrouselUp"), "80%", "85%", 40, 3, false, "up");
         carrouselMainExt.createCarrousel();
         carrouselMainExt.start();
 
